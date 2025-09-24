b5dee0ca6788aac8e40958f962c275a08413e3bf
olukayodeshodunke-droid <olukayode.shodunke@ontariotechu.net>
2025-09-24 17:51:50 -0400

    Update lab01.py

--- lab01.py
+++ lab01.py
@@ -1,26 +1,28 @@
 def main():
+    
     cost_per_item = 19.99
     quantity = 5 
     subtotal_cost = 99.95
     tax = 12.99
     total_cost = 112.94
 
+
     # YOUR CODE FOR PART 1 GOES HERE 
-    # cost_per_item * quantity = subtotal_cost
-    # tax = subtotal_cost * 0.13
-    # total_cost = subtotal + tax
+    cost_per_item = 19.99
+    quantity = 5 
+    subtotal_cost = cost_per_item * quantity
+    tax = subtotal_cost * 0.13
+    total_cost = subtotal_cost + tax
 
 
     # YOUR CODE FOR PART 2 GOES HERE
-    print(f'cost_per_item = ${19.99:0.2f}:\n')
-    print(f'quantity = {5}:\n')
-    print(f'subtotal_cost = ${99.95:0.2f}:\n')
-    print(f'tax = ${12.99:0.2f}:\n')
-    print(f'total_cost = ${112.94:0.2f}:\nAfter 5 years, your investment will be worth 1187.6863056468749 dollars.\n')
+    print(f'cost_per_item = ${cost_per_item:0.2f}:')
+    print(f'quantity = {quantity}:')
+    print(f'subtotal_cost = ${subtotal_cost:0.2f}:')
+    print(f'tax = ${tax:0.2f}:')
+    print(f'total_cost = ${total_cost:0.2f}:')
     
-
-
-
+    
     # THIS IS THE CODE FOR PART 3
     # initial_investment = 1000
     # interest_rate = 0.035
@@ -30,7 +32,7 @@ def main():
     # investment += investment * interest_rate
     # investment += investment * interest_rate
     # investment += investment * interest_rate
-    # print('After 5 years, your investment will be worth ' + investment + ' dollars.')
+    # print(f'After 5 years, your investment will be worth ' + investment + ' dollars.')
     # expected output: After 5 years, your investment will be worth 1187.6863056468749 dollars.
 
 if __name__ == "__main__":
